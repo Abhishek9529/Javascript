16ab726d19c8f65bcc1352e641a8a6f31e75324a
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-08 19:08:10 +0530

    Modified File Loops.js & Add 2 Files : Index.html And String.js

--- Loops.js
+++ Loops.js
@@ -1,6 +1,89 @@
 //  Loops In Javascript 
 //  1. for loop
 
-for (let i =0 ; i< 5; i++){
-    console.log("Hello Coder");
-}
+// for (let i =0 ; i< 5; i++){
+//     console.log(i);
+// }
+
+// print 1 to 10 natural numbers
+// let num = 10 ;
+// for (let i = 1; i <= num; i++){
+//     console.log(i);
+// }
+
+// print 1 to 10 Even  numbers
+// let num = 10 ;
+// for (let i = 1; i <= num; i++){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+// print 1 to 10 Odd  numbers
+// let num = 10 ;
+// for (let i = 1; i <= num; i++){
+//     if (i % 2 != 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// While Loop
+// let num = 1 ;
+// while (num <= 10){
+//     console.log("Hello Coder");
+//     num ++;
+// }
+
+
+// Do While Loop
+// let num = 1 ;
+// do {
+//     console.log("Hello Coder");
+//     num ++;
+// }
+// while(num <= 10);
+
+
+// For of loop 
+// let name = "Abhishek";
+// for (let val of name){
+//     console.log(val);
+// }
+
+
+// For in Loop
+// const student = {
+//     name : "Raj",
+//     class : "3rd",
+//     marks : 60,
+//     city : "pune"
+// }
+
+// for (let key in student)
+// {
+//     console.log(key);
+// }
+
+
+// Qs1. Print all even numbers from 0 to 100.
+// for (let i = 0; i<= 100; i++){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// Qs2. Create a game where you start with any random game number. Ask the user to keep
+//  guessing the game number until the user enters correct value.
+
+// let num = 7;
+// while (true){
+//     let inNum = prompt("Enter Any Number : ");
+//     if (inNum == num){
+//         console.log("Congurates You Enter Correct Value");
+//         break;
+//     }
+// }
+
---
+++ String.js
@@ -0,0 +1,45 @@
+// String in JS
+//  String is a sequence of characters used to represent text
+let str = "Abhishek";
+let str2 = " Chavhan";
+// console.log(str);
+
+// iterating string using for of loop
+// for (let val of str){
+//     console.log(val);
+// }
+
+// Concate two String
+// concate_string = str + str2;
+// console.log(concate_string);
+
+// String Lenght 
+// console.log(str.length);
+
+// String Indices
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+// console.log(str[3]);
+
+//  String Interpolation
+// let age = 28;
+// console.log(`Rahul Age is : ${age}`);
+
+
+// String Methods in JS
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str2.trim());
+
+// console.log(str.slice(0, 4));
+// console.log(str2.replace(str));
+// console.log(str.charAt(0));
+
+
+
+
+
+
+
+
