1bd309f01a301423c6d26e2d5dfe0f0ce3c2f9f5
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-12 21:52:56 +0530

    Add String Questions File

---
+++ StringQuestions.js
@@ -0,0 +1,60 @@
+// Strings (10 Questions)
+
+// 1 Question: Write a program to find the length of a string.
+// let str = "Abhi";
+// console.log(str.length);
+
+// 2 Question: Create a program to convert a string to uppercase and lowercase.
+// let str = "Abhishek";
+// console.log(`To UpperCase : ${str.toUpperCase()}`)
+// console.log(`To UpperCase : ${str.toLowerCase()}`)
+
+
+// 4 Question: Use a loop to count the number of vowels in a string.
+// Question: Write a program to reverse a string.
+
+// function reversedString(str){
+//     let charArray = str.split('');
+//     let reversedArray = charArray.reverse();
+
+//     return reversedArray.join('');
+// }
+
+// let originalString = "Abhishek";
+// let reversed_String = reversedString(originalString);
+// console.log(originalString);
+// console.log(reversed_String);
+
+
+// Question: Create a program to replace all instances of a word in a string using replace().
+// function replaceAllInstances(sentence, oldWord, newWord){
+//     return sentence.replaceAll(oldWord, newWord);
+// }
+
+// let sentence = "The quick brown fox jumps over the lazy dog. The dog is happy.";
+// let updatedSentence = replaceAllInstances(sentence, "dog", "cat");
+
+// console.log(`Original Sentence : ${sentence}`);
+// console.log(`Updated Sentence : ${updatedSentence}`);
+
+
+// Question: Write a program to extract the first 5 characters from a string using substring().
+// let str = "Abhishek";
+// console.log(str.substring(0,5));
+
+// Question: Create a program to split a string into an array of words.
+// let str = "Abhishek";
+// console.log(str);
+// let arr = str.split('');
+// console.log(arr);
+
+// Question: Check if a string starts with or ends with a specific word using startsWith() and endsWith().
+// let str = "Abhishek";
+// console.log(str.startsWith('A'));
+// console.log(str.endsWith('k'));
+
+// Question: Write a program to concatenate two strings and print the result.
+// let str1 = "Abhi";
+// let str2 = " Chavhan";
+// let result = str1 + str2;
+// console.log(result);
