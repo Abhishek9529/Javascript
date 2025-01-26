b7ae68e305be7e4be35767b1eba1d423c88fb60c
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-01-26 21:55:20 +0530

    Modified Files

---
+++ Functions/script.js
@@ -0,0 +1,61 @@
+
+
+// sayMyName("Abhi", "Chavhan")
+// // function uithout using return 
+// function sayMyName(firstName, secondName){
+//    console.log(firstName, " ", secondName);
+// }
+
+
+// function using return 
+
+// console.log(sayMyName("Abhi", "Chavhan"));
+// const fullName = sayMyName("Abhi", "Chavhan")
+// console.log(fullName);
+
+// function sayMyName(firstName, secondName){
+//    return firstName + " " + secondName;
+// }
+
+
+// function loginUserMessage(username = "Ram"){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${username} just logged in` 
+// }
+// console.log(loginUserMessage());
+
+
+// function with multiple parameter
+// function calculaterCartPrice( ...allVal){ // this ... operator return all argrument in one var
+//     return  allVal;
+// }
+// console.log(calculaterCartPrice(100, 200, 300, 400));
+
+
+// use function to handle object
+
+// const  user = {
+//     username : "abhi",
+//     prices : 199
+// }
+
+// function handleObject (anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+
+// handleObject({
+//     username : "sam",
+//     price: 399
+// })
+
+
+// const myNewArray = [200, 400, 100, 600]
+
+// function getArrayVal(myArr){
+//     return myArr[2]
+// }
+// console.log(getArrayVal(myNewArray));
+
