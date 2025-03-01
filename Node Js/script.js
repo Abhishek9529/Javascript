e96dc8925e431c93a795a3eb3febb85d9f87ace1
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-03-01 22:35:38 +0530

    Add new Files

---
+++ Node Js/script.js
@@ -0,0 +1,55 @@
+// const fs = require('fs');
+// fs.writeFile("test.txt", "this is new file", function(err){
+//     if(err){console.log(err);
+//     }else{
+//         console.log("Done...");
+//     }
+// })
+
+
+// create new file with content
+// const fs = require('fs');
+// fs.writeFile("Test2.txt", "This is Test2 file", (err)=>{
+// if(err){
+//     console.log(err);
+// }
+// else{
+//     console.log("Done...");
+// }
+// })
+
+// append new text to previous file
+const fs = require("fs");
+// fs.appendFile("Text2.txt", "hey javascript", (err) =>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Done ....");
+//     }
+// })
+
+// rename file
+// fs.rename("test.txt","newText.txt",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Done...");
+//     }
+// })
+
+// copy file
+// fs.copyFile("newText.txt", "./newFolder/copyText.txt",(err)=>{
+//     if (err) {
+//         console.log(err); 
+//     }else{
+//         console.log("Done...");
+//     }
+// })
+
+// rmdir
+// fs.rmdir("./newFolder",{recursive: true}, (err)=>{
+//    if(err)  console.log(err);
+//    else console.log("folder removed...");
+// })
+
